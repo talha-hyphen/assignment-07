58722978e18c73889da23c245e2ff09b7530cb42
talha-hyphen <w00052207@gmail.com>
2024-12-15 07:04:05 -0800

    assignment 07

---
+++ script.js
@@ -0,0 +1,63 @@
+
+ 
+
+
+
+
+//9. Weather Suggestion Experiment
+
+//var userInput = prompt("Suggestions for weather conditions either input Sunny, Rainy, Cold")
+//if(userInput=="Sunny"){console.log("wear sunglasses")}
+//else if(userInput=="Rainy"){console.log("Take an umbrella ")}
+//else if(userInput=="Cold"){console.log("Wear a jacket ")}
+ 
+
+
+
+
+
+
+//3. Water Temperature Checker
+
+
+//var userInput = prompt("enter  temperature in Celsius")
+//if (userInput<=0) {console.log("freezing")}
+//else if (userInput >= 1 && userInput <= 15)
+//{console.log("cold")}
+//else if (userInput >= 16 && userInput <= 30)
+  //{console.log("warm")}
+//else  (userInput >=31  )
+ // {console.log("Hot")}
+
+
+
+//5. Simple Alarm System( Simulate an alarm trigger based on conditions.)
+//var userInput = prompt("Is the door open? (yes or no)")
+//var username = prompt("Is motion detected? (yes or no) ")
+//if (userInput=="yes||no") {
+  //console.log("username")}
+//else if("userinput==yes||username==yes")
+ //   {console.log("Alarm Trigged")}
+//else ("userInput==no&&username==no")
+  // {console.log("All save")}
+
+
+
+
+//2. Traffic Light Simulator (Accept an input (Red, Yellow, Green) from the user.// 
+//var userInput = prompt("Enter Signal Color either red,yellow or green")
+//if (userInput=="red") {console.log("Stop")}
+//else if (userInput=="yellow") {console.log("Slow down")} 
+//else if (userInput=="green") {console.log("Go")}
+//else{console.log("Enter a valid color")}
+
+
+
+
+
+//1.Light bulb Experiment (program that asks the user to input either 1 (turn on) or 0 (turn off).//
+
+//var userInput = prompt("Enter a Number either 0 and 1");
+//if (userInput ==0) {console.log("Bulb is OFF")}
+//else if (userInput==1) { console.log("Bulb is ON")}
+//else{console.log("Enter a Valid number")}
